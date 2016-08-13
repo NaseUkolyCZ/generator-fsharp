e11d0879a1a61808b6ab9f5ef6245c227135c8d7
David Podhola <david.podhola@naseukoly.cz>
2016-08-13 18:31:32 +0200

    Application Block on Microsoft SQL Server
    Application Block on Microsoft SQL Server with Suave and Orleankka
    Application Block on Microsoft SQL Server with Suave,Orleankka and WebSharper

---
+++ appblockmssqlokkasuave/src/ApplicationName.Core/DomainTypes.fs
@@ -0,0 +1,13 @@
+﻿namespace ApplicationName.Core
+
+#if DEMO
+  [<CLIMutable>]
+  type Person = {
+      FirstName:string
+      LastName:string
+  }
+
+  type HelloMessage = 
+    | Greet of Person
+    | Hi
+#endif
